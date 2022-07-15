01cec0b99ddc4cd16a162590eba92e64d7500ebf
k0gary0ta <pilgrim.189060@gmail.com>
2022-07-15 23:12:54 +0900

    更新

--- app/pkg/auth.go
+++ app/pkg/auth.go
@@ -1,9 +1,15 @@
 package pkg
 
 import (
+	"crypto/rand"
+	"encoding/base64"
 	"fmt"
+	"io"
 	"log"
+	"net/http"
+	"net/url"
 	"os"
+	"sync"
 	"time"
 
 	jwtmiddleware "github.com/auth0/go-jwt-middleware"
@@ -68,18 +74,78 @@ var JWTMiddleware = jwtmiddleware.New(jwtmiddleware.Options{
 	},
 })
 
-// type Provider interface {
-// 	SessionInit(sid string) (Session, error)
-// 	SessionRead(sid string) (Session, error)
-// 	SessionDestroy(sid string) error
-// 	SessionGC(maxLifeTime int64)
-// }
+var globalSessions *session.Manager
 
-// type Manager struct {
-// 	cookieName string
-// 	lock sync.Mutex
-// 	provider Provider
-// 	maxlifetime int64
-// }
+type Session interface {
+	Set(key, value interface{}) error // set session value
+	Get(key interface{}) interface{}  // get session value
+	Delete(key interface{}) error     // delete session value
+	SessionID() string                // return current sessionID
+}
 
-// func ValidateCookie() {}
+type Provider interface {
+	SessionInit(sid string) (Session, error) // セッションの初期化
+	// Session変数を返す
+	// 存在しなければ新たにSession変数を作成して返す
+	SessionRead(sid string) (Session, error)
+	SessionDestroy(sid string) error // Session変数を破棄する
+	SessionGC(maxLifeTime int64)     // 期限の切れたSessionを破棄
+}
+
+type Manager struct {
+	cookieName  string     // private cookiename
+	lock        sync.Mutex // protects session
+	provider    Provider
+	maxlifetime int64
+}
+
+func NewManager(provideName, cookieName string, maxlifetime int64) (*Manager, error) {
+	provider, ok := provides[provideName]
+	if !ok {
+		return nil, fmt.Errorf("session: unknown provide %q (forgotten import?)", provideName)
+	}
+	return &Manager{provider: provider, cookieName: cookieName, maxlifetime: maxlifetime}, nil
+}
+
+var provides = make(map[string]Provider)
+
+func Register(name string, provider Provider) {
+	if provider == nil {
+		panic("session: Register provide is nil")
+	}
+	if _, dup := provides[name]; dup {
+		panic("session: Register called twice for provide " + name)
+	}
+	provides[name] = provider
+}
+
+func (manager *Manager) sessionId() string {
+	b := make([]byte, 32)
+	if _, err := io.ReadFull(rand.Reader, b); err != nil {
+		return ""
+	}
+	return base64.URLEncoding.EncodeToString(b)
+}
+
+func (manager *Manager) SessionStart(w http.ResponseWriter, r *http.Request) (session Session) {
+	manager.lock.Lock()
+	defer manager.lock.Unlock()
+	cookie, err := r.Cookie(manager.cookieName)
+	if err != nil || cookie.Value == "" {
+		sid := manager.sessionId()
+		session, _ := manager.provider.SessionInit(sid)
+		cookie := http.Cookie{
+			Name:     manager.cookieName,
+			Value:    url.QueryEscape(sid),
+			Path:     "/",
+			HttpOnly: true,
+			MaxAge:   int(manager.maxlifetime),
+		}
+		http.SetCookie(w, &cookie)
+		return session
+	} else {
+		sid, _ := url.QueryUnescape(cookie.Value)
+		session, _ := manager.provider.SessionRead(sid)
+		return session
+	}
+}
